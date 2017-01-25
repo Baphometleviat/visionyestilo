4fef8f6081e3553b22c294549c0bdb3b9f8bce61
Baphometleviat <baphometleviat@gmail.com>
2017-01-25 01:29:25 -0600

    Avanzamos
    Pequeños cambios, ya quedó el nav, falta hacerlo responsivo y crear el
    sitio en sí. Vamos bien.

---
+++ vyepublic/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="shortcut icon" href="css/opt_logo.jpg">
+    <title>Vision & Estilo Opticas</title>
+  </head>
+  <body>
+    <nav>
+      <ul>
+        <li><a href="">¿Quienes somos?</a></li>
+        <li><a href="">Nuestros servicios</a></li>
+        <li><a href="">Contacto</a></li>
+        <li><a href="">Catalogo</a></li>
+      </ul>
+    </nav>
+      <div class="header">
+        <div class="body-header">
+          VISION & ESTILO
+          <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        </div>
+      </div class="header">
+      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+  </body>
+</html>
