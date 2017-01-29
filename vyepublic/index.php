0cacd767d6779ee751ef958681cd603fd2359e53
Baphometleviat <baphometleviat@gmail.com>
2017-01-28 21:27:56 -0600

    Header
    El header quedó listo sólo falta hacerlo responsive y esperar a que Hugo
    pase el texto.

--- vyepublic/index.php
+++ vyepublic/index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Tangerine" rel="stylesheet">
     <link rel="stylesheet" href="css/general.css">
     <link rel="shortcut icon" href="css/opt_logo.jpg">
     <title>Vision & Estilo Opticas</title>
@@ -10,18 +11,24 @@
   <body>
     <nav>
       <ul>
-        <li><a href="">¿Quienes somos?</a></li>
+        <li><a href="#quienes-somos">¿Quienes somos?</a></li>
         <li><a href="">Nuestros servicios</a></li>
         <li><a href="">Contacto</a></li>
         <li><a href="">Catalogo</a></li>
       </ul>
     </nav>
       <div class="header">
-        <div class="body-header">
-          VISION & ESTILO
-          <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <div class="contenedor-header">
+          <div class="imagen-header">
+            <img src="css/ojo-logo.png" class="logo">
+          </div>
+          <div class="texto-header">
+              Vision & Estilo
+          </div>
         </div>
       </div class="header">
-      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <div class="quienes-somos" id="quienes-somos">
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      </div>
   </body>
 </html>
